b263830ec85eea5b4b0aa7820d3fd0975bb5bf32
Alexander Nichiporenko <alexvn.home@gmail.com>
2021-03-15 15:49:57 +0300

    Lesson-2. Gracefully shutdown server

--- working/main.go
+++ working/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 
 	"github.com/mwazovzky/microservices-introduction/working/handlers"
@@ -26,6 +28,22 @@ func main() {
 		WriteTimeout: 1 * time.Second,
 	}
 
-	log.Println("Starting http server at :9090")
-	server.ListenAndServe()
+	go func() {
+		log.Println("Starting http server at :9090")
+		err := server.ListenAndServe()
+		if err != nil {
+			logger.Fatal(err)
+		}
+	}()
+
+	// gracefully shutdown the server allows to complete current request
+	sigChan := make(chan os.Signal)
+	// broadcast operating system signals to the channel
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+	// wait for the signal
+	sig := <-sigChan
+	logger.Println("Recieved terminate signal, graceful shutdown", sig)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	server.Shutdown(ctx)
 }
