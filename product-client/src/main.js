493fad5f5033489389743bc45e4eac998b677929
Alexander Nichiporenko <alexvn.home@gmail.com>
2021-03-17 11:43:49 +0300

    Lesson-9. Setup http service

--- product-client/src/main.js
+++ product-client/src/main.js
@@ -2,8 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import http from "./plugins/http";
 
 createApp(App)
   .use(store)
   .use(router)
+  .use(http)
   .mount("#app");
